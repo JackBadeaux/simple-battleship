7fba0d0802ab9c51a0ac5cd348c18444e905145c
JackBadeaux <jack.badeaux@mscoding.org>
2025-05-19 17:00:42 -0500

    more styling and reset button
    added images to show when ship is sunk and when missed

--- js/app.js
+++ js/app.js
@@ -1,14 +1,26 @@
-const squares = document.querySelectorAll(".grid-square")
-const correctIndex = Math.floor(Math.random() * 9 )
-squares.forEach((square,index) => {
-    square.addEventListener("click",function(){
-       if (index === correctIndex) {
-         square.classList.add("correct")
-       } else {
-        square.classList.add("incorrect")
-       }
-    })
+//! varibales
+const squares = document.querySelectorAll(".grid-square");
+const correctIndex = Math.floor(Math.random() * 9);
+let gameOver = false; 
+//! game logic
+squares.forEach((square, index) => {
+    square.addEventListener("click", function () {
+        if (gameOver) return; 
+
+        if (index === correctIndex) {
+            square.classList.add("correct", "sunk");
+            gameOver = true;
+            setTimeout(() =>{
+            }, 100);
+
+        } else {
+            square.classList.add("incorrect", "miss");
+        }
+    });
 });
 
-
+//! reset logic
+document.getElementById("resetButton").addEventListener("click", function(){
+location.reload();
+})
 
