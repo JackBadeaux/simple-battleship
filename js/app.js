4d5680da1bed9cf8c3b991c2483c92a027e32be4
JackBadeaux <jack.badeaux@mscoding.org>
2025-05-19 10:30:44 -0500

    finished project

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
-const squares = document.querySelectorAll("grid-square")
-
+const squares = document.querySelectorAll(".grid-square")
+const correctIndex = Math.floor(Math.random() * 9 )
+squares.forEach((square,index) => {
+    square.addEventListener("click",function(){
+       if (index === correctIndex) {
+         square.classList.add("correct")
+       } else {
+        square.classList.add("incorrect")
+       }
+    })
+});
 
 
 
