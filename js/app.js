85ca6e245654ec3ea8e2336a63b125602de0b70c
JackBadeaux <jack.badeaux@mscoding.org>
2025-05-19 23:34:24 -0500

    added bomb count

--- js/app.js
+++ js/app.js
@@ -1,26 +1,39 @@
 //! varibales
 const squares = document.querySelectorAll(".grid-square");
 const correctIndex = Math.floor(Math.random() * 9);
-let gameOver = false; 
+const livesDisplay = document.getElementById("lives");
+let gameOver = false;
+let remainingBombs = 0;
 //! game logic
 squares.forEach((square, index) => {
     square.addEventListener("click", function () {
-        if (gameOver) return; 
-
+        if (gameOver || square.classList.contains("miss") || square.classList.contains("sunk")) return;
+        remainingBombs--;
         if (index === correctIndex) {
             square.classList.add("correct", "sunk");
             gameOver = true;
-            setTimeout(() =>{
+            setTimeout(() => {
+                alert("Good Job! You have won")
+                location.reload();
             }, 100);
-
+            return
         } else {
             square.classList.add("incorrect", "miss");
+
+            livesDisplay.textContent = `Bombs: ${remainingBombs}`
+        }
+        if (remainingBombs <= 0) {
+            gameOver = true
+            setTimeout(() => {
+                alert("Game Over out of bombs")
+                location.reload();
+            }, 100);
+
         }
     });
 });
-
 //! reset logic
-document.getElementById("resetButton").addEventListener("click", function(){
-location.reload();
+document.getElementById("resetButton").addEventListener("click", function () {
+    location.reload();
 })
 
